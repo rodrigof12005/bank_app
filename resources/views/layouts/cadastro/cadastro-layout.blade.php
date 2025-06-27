a645160a2889c5f2c8b2f508d1df509223aaf725
“Rodrigo <rodrigo@coffee-code.tech>
2025-06-27 10:03:22 -0300

    Correção no layout de cadastro

--- resources/views/layouts/cadastro/cadastro-layout.blade.php
+++ resources/views/layouts/cadastro/cadastro-layout.blade.php
@@ -12,7 +12,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Minha App</a>
+                <a class="navbar-brand" href="#">Bank APP</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -22,7 +22,7 @@
                             <a class="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/cadastro">Cadastro</a>
+                            <a class="nav-link" href="/novo-cadastro">Cadastro</a>
                         </li>
                     </ul>
                 </div>
